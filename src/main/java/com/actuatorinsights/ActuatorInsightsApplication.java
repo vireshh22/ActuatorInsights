1a662136d49ba71544c1dbc75dd46fc95e612b80
vireshh22 <viresh0489@gmail.com>
2025-03-29 14:56:44 +0530

    project setup

---
+++ src/main/java/com/actuatorinsights/ActuatorInsightsApplication.java
@@ -0,0 +1,13 @@
+package com.actuatorinsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorInsightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actuatorinsights/ActuatorInsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actuatorinsights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorInsightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
